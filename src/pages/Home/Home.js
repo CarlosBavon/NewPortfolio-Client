7c3d1da048e38a52c1d4ecb351f41674b7e5e933
CarlosBavon <carlosbavon46@gmail.com>
2026-06-08 21:54:45 +0300

    Changing meta title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import axios from 'axios';
+import api from '../../services/api';
 import { useToast } from '../../hooks/useToast';
 import './Home.css';
 
